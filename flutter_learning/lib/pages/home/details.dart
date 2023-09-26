0ac750db4ee16aa3a317fb8c997fa1d4291c20d6
ederson23 <ederson@webfloat.com.br>
2023-09-26 17:47:55 -0300

    fix: overflow em detalhes

--- flutter_learning/lib/pages/home/details.dart
+++ flutter_learning/lib/pages/home/details.dart
@@ -37,86 +37,117 @@ class _DetailsState extends State<Details> {
         ),
         body: Stack(
           children: [
-            Container(
-              margin: const EdgeInsets.only(top: 300),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(top: 300),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
                 ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 20),
-                    child: const TabBar(
-                      tabs: [
-                        Tab(text: 'Sobre'),
-                        Tab(text: 'Evoluções'),
-                        Tab(text: 'Movimentos'),
-                      ],
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 20),
+                      child: const TabBar(
+                        tabs: [
+                          Tab(
+                            child: Text(
+                              'Sobre',
+                              style: TextStyle(
+                                  color:
+                                      Colors.black), // Set text color to black
+                            ),
+                          ),
+                          Tab(
+                            child: Text(
+                              'Evoluções',
+                              style: TextStyle(
+                                  color:
+                                      Colors.black), // Set text color to black
+                            ),
+                          ),
+                          Tab(
+                            child: Text(
+                              'Movimentos',
+                              style: TextStyle(
+                                  color:
+                                      Colors.black), // Set text color to black
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const Expanded(
-                    child: TabBarView(
-                      children: [
-                        Column(
-                          children: [
-                            ListTile(
-                              title: Text('Nome: Charmander'),
-                              subtitle: Text('Tipo: Fogo'),
+                    const Expanded(
+                      child: TabBarView(
+                        children: [
+                          SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Nome: Charmander'),
+                                  subtitle: Text('Tipo: Fogo'),
+                                ),
+                                ListTile(
+                                  title: Text('Altura: 0.6m'),
+                                  subtitle: Text('Peso: 8.5kg'),
+                                ),
+                                ProgressBar(
+                                    name: 'HP',
+                                    currentValue: 45,
+                                    maxValue: 255),
+                                ProgressBar(
+                                    name: 'Ataque',
+                                    currentValue: 224,
+                                    maxValue: 255),
+                                ProgressBar(
+                                    name: 'Defesa',
+                                    currentValue: 85,
+                                    maxValue: 255),
+                                ProgressBar(
+                                    name: 'Velocidade',
+                                    currentValue: 100,
+                                    maxValue: 255),
+                                ProgressBar(
+                                    name: 'Total',
+                                    currentValue: 180,
+                                    maxValue: 255),
+                              ],
                             ),
-                            ListTile(
-                              title: Text('Altura: 0.6m'),
-                              subtitle: Text('Peso: 8.5kg'),
+                          ),
+                          SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Evo 1: Charmeleon'),
+                                  subtitle: Text('Fogo'),
+                                ),
+                                ListTile(
+                                  title: Text('Evo 2: Charizard'),
+                                  subtitle: Text('Fogo'),
+                                ),
+                              ],
                             ),
-                            ProgressBar(
-                                name: 'HP', currentValue: 45, maxValue: 255),
-                            ProgressBar(
-                                name: 'Ataque',
-                                currentValue: 224,
-                                maxValue: 255),
-                            ProgressBar(
-                                name: 'Defesa',
-                                currentValue: 85,
-                                maxValue: 255),
-                            ProgressBar(
-                                name: 'Velocidade',
-                                currentValue: 100,
-                                maxValue: 255),
-                            ProgressBar(
-                                name: 'Total',
-                                currentValue: 180,
-                                maxValue: 255),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            ListTile(
-                              title: Text('Evo 1: Charmeleon'),
-                              subtitle: Text('Fogo'),
+                          ),
+                          SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Move 1: Ember'),
+                                ),
+                                ListTile(
+                                  title: Text('Move 2: Flamethrower'),
+                                )
+                              ],
                             ),
-                            ListTile(
-                              title: Text('Evo 2: Charizard'),
-                              subtitle: Text('Fogo'),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            ListTile(
-                              title: Text('Move 1: Ember'),
-                            ),
-                            ListTile(
-                              title: Text('Move 2: Flamethrower'),
-                            )
-                          ],
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             const Positioned(
@@ -127,8 +158,8 @@ class _DetailsState extends State<Details> {
                 'Charmander',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Colors.white, // Set text color to white
-                  fontSize: 24.0, // Set font size to 24
+                  color: Colors.white,
+                  fontSize: 24.0,
                 ),
               ),
             ),
